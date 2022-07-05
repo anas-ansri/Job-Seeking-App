e3ecc91bbfde6c9207cbccc8f2f97861025bc2ea
anasalansari <77924312+anasalansari@users.noreply.github.com>
2022-07-05 20:41:12 +0530

    Almost Done

--- lib/screen/home/edit_page.dart
+++ lib/screen/home/edit_page.dart
@@ -1,6 +1,7 @@
 import 'package:ad_project/models/user.dart';
 import 'package:ad_project/services/database.dart';
 import 'package:ad_project/utils/constants.dart';
+import 'package:ad_project/utils/helper/list_item.dart';
 import 'package:ad_project/utils/helper/profile_widget.dart';
 import 'package:ad_project/utils/helper/rounded_button.dart';
 import 'package:ad_project/utils/helper/text_field_widget.dart';
@@ -21,11 +22,18 @@ class EditPage extends StatefulWidget {
 class _EditPageState extends State<EditPage> {
   String _newName = "";
   String _newPhotoUrl = "";
+  String? selectedItem = "None";
+  String _newEmail = "";
+  final listKey = GlobalKey<AnimatedListState>();
+  // final List<String> jobList = widget.userData!.job_prefs;
+  // List<String> jobList = List.from(widget.userData!.job_prefs);
 
   @override
   Widget build(BuildContext context) {
+    UserData userData = widget.userData as UserData;
     String uid = FirebaseAuth.instance.currentUser!.uid;
     DatabaseService db = DatabaseService(uid: uid);
+    List<String> jobList = userData.job_prefs;
 
     return Scaffold(
         appBar: AppBar(
@@ -36,14 +44,15 @@ class _EditPageState extends State<EditPage> {
         body: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 32),
           physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
           children: [
             const SizedBox(
               height: 60,
             ),
             EditProfileWidget(
-              imagePath: (_newPhotoUrl == ""
-                  ? widget.userData!.photoUrl
-                  : _newPhotoUrl),
+              imagePath:
+                  (_newPhotoUrl == "" ? userData.photoUrl : _newPhotoUrl),
               isEdit: true,
               onClicked: () async {
                 String downloadURL;
@@ -86,22 +95,77 @@ class _EditPageState extends State<EditPage> {
               readOnly: false,
               label: 'Email',
               text: widget.userData!.email,
-              onChanged: (email) {},
+              onChanged: (email) {
+                setState(() {
+                  _newEmail = email;
+                });
+              },
             ),
-            // const SizedBox(
-            //   height: 10,
-            // ),
-            // Text(
-            //   "You can not modify email",
-            //   textAlign: TextAlign.center,
-            //   style: TextStyle(color: Colors.red[300]),
-            // ),
             const SizedBox(height: 24),
-            TextFieldWidget(
-              readOnly: true,
-              label: 'Edit job preferences',
-              text: widget.userData!.email,
-              onChanged: (email) {},
+            Text(
+              "Edit job preferences",
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            const SizedBox(height: 24),
+            AnimatedList(
+              key: listKey,
+              shrinkWrap: true,
+              initialItemCount: jobList.length,
+              itemBuilder: (BuildContext context, int index,
+                  Animation<double> animation) {
+                return ListItemWidget(
+                  text: jobList[index],
+                  animation: animation,
+                  onClicked: () {
+                    final removedItem = jobList[index];
+                    jobList.removeAt(index);
+
+                    listKey.currentState!.removeItem(
+                        index,
+                        (context, animation) => ListItemWidget(
+                            text: removedItem, animation: animation));
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 24),
+            Column(
+              children: [
+                FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: DropdownButton<String>(
+                    value: selectedItem,
+                    items: allJobFunction
+                        .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              // style: TextStyle(fontSize: 10),
+                            )))
+                        .toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        selectedItem = val;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      final newIndex = jobList.length;
+                      if (selectedItem == "None") {
+                        showAlertDialog(context, "",
+                            "Please select a job preference", false);
+                      } else {
+                        jobList.insert(newIndex, selectedItem!);
+                        listKey.currentState!.insertItem(newIndex);
+                      }
+                    },
+                    child: Text("Add"))
+              ],
             ),
             const SizedBox(height: 24),
             RoundedButton(
@@ -114,7 +178,17 @@ class _EditPageState extends State<EditPage> {
                   if (_newPhotoUrl != "") {
                     await db.updateUserPic(_newPhotoUrl);
                   }
-                  Navigator.of(context).pop();
+                  if (_newEmail != "") {
+                    await db.updateEmail(_newEmail);
+                  }
+                  if (jobList.length > 5 || jobList.length < 2) {
+                    showErrorAlert(
+                        context, "Please select between 2 to 5 job functions");
+                  } else {
+                    await db.updateJobPref(jobList);
+                    showAlertDialog(context, "Thank you",
+                        "Your details are updated successfully.", true);
+                  }
                 })
           ],
         ));
